c33249f7f699752198c4b232faf23926cb88783d
Dženan Zukić <dzenan.zukic@kitware.com>
2020-10-09 15:57:26 -0400

    Make ANDOR_ prefix consistently uppercase and with underscore
    This automatically groups the options is CMake

--- Modules/FindAndor.cmake
+++ Modules/FindAndor.cmake
@@ -3,14 +3,12 @@
 # ANDOR_INCLUDE_DIR - where to find the header files
 # ANDOR_LIBRARY_DIR - libraries to be linked
 # ANDOR_BINARY_DIR - common shared libraries to be installed
-# ANDOR_WIN32_BINARY_DIR - 32-bit shared libraries to be installed
-# ANDOR_WIN64_BINARY_DIR - 64-bit shared libraries to be installed
 
 SET( ANDOR_PATH_HINTS
   "C:/Program Files/Andor SDK"
   )
 
-FIND_PATH(Andor_INCLUDE_DIR ATMCD32D.h
+FIND_PATH(ANDOR_INCLUDE_DIR ATMCD32D.h
   DOC "ANDOR include directory (contains ATMCD32D.h)"
   PATHS ${ANDOR_PATH_HINTS}
   )  
@@ -21,33 +19,24 @@ else()
   set(bitness 32)
 endif()
 
-set(AndorLibName "atmcd${bitness}m") # suffix m is for Microsoft compiler, d is for Borland
-FIND_PATH(ANDOR_WIN_LIBRARY_DIR ${AndorLibName}${CMAKE_STATIC_LIBRARY_SUFFIX}
-  DOC "ANDOR ${bitness}-bit library directory (contains ${AndorLibName}${CMAKE_STATIC_LIBRARY_SUFFIX}})"
+set(ANDOR_LIBRARY "atmcd${bitness}m") # suffix m is for Microsoft compiler, d is for Borland
+FIND_PATH(ANDOR_LIBRARY_DIR ${ANDOR_LIBRARY}${CMAKE_STATIC_LIBRARY_SUFFIX}
+  DOC "ANDOR ${bitness}-bit library directory (contains ${ANDOR_LIBRARY}${CMAKE_STATIC_LIBRARY_SUFFIX}})"
   PATHS ${ANDOR_PATH_HINTS}
   )
 
-set(AndorDLLName "atmcd${bitness}d")
-FIND_PATH(ANDOR_WIN_BINARY_DIR ${AndorDLLName}${CMAKE_SHARED_LIBRARY_SUFFIX}
-  DOC "Path to ANDOR ${bitness}-bit binary directory (contains ${bitness}-bit ${AndorDLLName}.dll)"
+set(ANDOR_DLL "atmcd${bitness}d")
+FIND_PATH(ANDOR_BINARY_DIR ${ANDOR_DLL}${CMAKE_SHARED_LIBRARY_SUFFIX}
+  DOC "Path to ANDOR ${bitness}-bit binary directory (contains ${bitness}-bit ${ANDOR_DLL}.dll)"
   PATHS ${ANDOR_PATH_HINTS}
   NO_DEFAULT_PATH # avoid finding installed DLLs in the system folders
   )
-  
-set( Andor_BINARY_DIR ${ANDOR_WIN_BINARY_DIR} )
-set( Andor_LIBRARY_DIR ${ANDOR_WIN_LIBRARY_DIR} )
 
 # handle the QUIETLY and REQUIRED arguments and set ANDOR_FOUND to TRUE if
 # all listed variables are TRUE
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(ANDOR DEFAULT_MSG
-  Andor_LIBRARY_DIR
-  Andor_INCLUDE_DIR
-  Andor_BINARY_DIR
+  ANDOR_LIBRARY_DIR
+  ANDOR_INCLUDE_DIR
+  ANDOR_BINARY_DIR
   )
-
-IF(ANDOR_FOUND)
-  SET( ANDOR_LIBRARY_DIR ${Andor_LIBRARY_DIR} )
-  SET( ANDOR_INCLUDE_DIR ${Andor_INCLUDE_DIR} )
-  SET( ANDOR_BINARY_DIR ${Andor_BINARY_DIR} )
-ENDIF()
